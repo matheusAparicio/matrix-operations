3a885817607efa52333fdc2ddf0b1dd9d4f6832b
Matheus <matheusilva-m@outlook.com>
2022-05-28 15:23:19 -0300

    Scan methods changes, matrices being stored

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ from scan import Scan
 scan = Scan("example.txt")
 
 if __name__ == '__main__':
-    scan.findMatrix()
-    scan.saveMatrix()
+    scan.saveMatrices()
--- scan.py
+++ scan.py
@@ -6,18 +6,30 @@ class Scan:
 
     def __init__(self, path: str):
         self.file = open(path, "r").readlines()
+        self.matrix0 = []
         self.matrix1 = []
-        self.matrix2 = []
-        self.matrixResult = []
+        self.matrixRealResult = []
+        self.matrixFileResult = []
         self.currentLine = 0
-        self.maxSearchLines = 10
+        self.currentMatrix = 0 # 0 = self.matrix0, 1 = self.matrix1, 2 = self.matrixResult
+        self.foundMatrix = False
 
-    def findMatrix(self):
-        for i, lineContent in enumerate(self.file):
+    def saveMatrices(self):
+        while self.currentLine < len(self.file):
+            lineContent = self.file[self.currentLine]
             if isNumber(lineContent[0]):
-                self.currentLine = i
-                return 0
+                self.foundMatrix = True
+                if lineContent[-1] == '\n':
+                    lineContent = lineContent[:-1]
+                if lineContent[-1] == ' ':
+                    lineContent = lineContent[:-1]
+                if self.currentMatrix == 0:
+                    self.matrix0.append(lineContent)
+                elif self.currentMatrix == 1:
+                    self.matrix1.append(lineContent)
+                else:
+                    self.matrixFileResult.append(lineContent)
             self.currentLine += 1
-
-    def saveMatrix(self):
-        print(self.currentLine)
+            if self.foundMatrix and self.currentLine < len(self.file) and not isNumber(self.file[self.currentLine][0]):
+                self.foundMatrix = False
+                self.currentMatrix += 1
